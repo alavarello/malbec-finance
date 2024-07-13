7c7bb815763af89540e921dcff19925aaf72fffa
Juan <jm@guide42.com>
2024-07-13 18:00:55 -0300

    UI Positions Hook

---
+++ ui/src/hooks/usePositions.js
@@ -0,0 +1,40 @@
+import { LENDING_CONDITIONS } from '../constants/lending'
+
+export default function usePositions({ chainId, address }) {
+  return {
+    positions: [
+      {
+        pool: {
+          poolId: '0x0000',
+          currency0: 'DAI',
+          currency1: 'USDC',
+        },
+        colateral: {
+          currency: 'ETH',
+          value: '1000',
+          liquidation: {
+            price: '3500000000000000000000000',
+            lendingCondition: LENDING_CONDITIONS.Long,
+          },
+        },
+        debt: {
+          currency: 'DAI',
+          value: '10',
+          liquidation: {
+            price: '100000000000000000000',
+            lendingCondition: LENDING_CONDITIONS.Short,
+          },
+        },
+        interest: {
+          rate: null,
+          fee: {
+            currency: 'ETH',
+            value: '100000000000000000',
+          },
+        },
+      },
+    ],
+    loading: false,
+    error: null,
+  }
+}
