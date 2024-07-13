a06f6c60d6a155169a2179f82f9a5dbe26289ac1
Juan <jm@guide42.com>
2024-07-13 19:28:58 -0300

    UI Lend

--- ui/src/constants/exchanges.js
+++ ui/src/constants/exchanges.js
@@ -2,3 +2,8 @@ export const EXCHANGES = {
   UNISWAP: 'UNISWAP',
   PANCAKESWAP: 'PANCAKESWAP',
 }
+
+export const EXCHANGES_DOMAIN = {
+  [EXCHANGES.UNISWAP]: 'https://app.uniswap.org',
+  [EXCHANGES.PANCAKESWAP]: 'https://pancakeswap.finance',
+}
