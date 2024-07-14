77d9685474b5601625cc0a3c7c4fc211b6d599c4
Juan <jm@guide42.com>
2024-07-14 03:35:57 -0300

    Fix Liquidity

--- ui/src/utils/liquidity.js
+++ ui/src/utils/liquidity.js
@@ -1,14 +1,18 @@
+import { parseUnits } from 'ethers'
 import { LENDING_CONDITIONS } from '../constants/lending'
+import { COINS } from '../constants/coins'
 
 export function calculateAvailableTokens(pool, targetPrice, lendingCondition, token) {
   return pool.ticks.reduce((availableTokens, tick) => {
+    const currency = token === pool.currency0 ? pool.currency0 : pool.currency1
     const price = parseFloat(token === pool.currency0 ? tick.price0 : tick.price1);
     if (
       lendingCondition === LENDING_CONDITIONS.Short && targetPrice <= price ||
       lendingCondition === LENDING_CONDITIONS.Long && targetPrice >= price
     ) {
-      return availableTokens + parseFloat(tick.liquidityNet)
+      console.debug(tick.liquidityNet)
+      return availableTokens + parseUnits(tick.liquidityNet, COINS[currency]?.decimals ?? 18)
     }
     return availableTokens
-  })
+  }, 0n)
 }
